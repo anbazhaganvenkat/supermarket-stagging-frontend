1080c73fc6b69aab71b59805eeea304c3e01b1e1
anbu <avenakateasn@thidiff.com>
2020-02-02 02:54:32 +0530

    sample react table

--- src/views/OrderList/OrderList.js
+++ src/views/OrderList/OrderList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class OrderList extends Component {
   constructor(props) {
@@ -8,10 +9,43 @@ class OrderList extends Component {
 
   render() {
 
+    const data = [{
+      name: 'Ayaan',
+      age: 26
+    },{
+      name: 'Ahana',
+      age: 22
+    },{
+      name: 'Peter',
+      age: 40
+    },{
+      name: 'Virat',
+      age: 30
+    },{
+      name: 'Rohit',
+      age: 32
+    },{
+      name: 'Dhoni',
+      age: 37
+    }]
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name'
+    },{
+      Header: 'Age',
+      accessor: 'age'
+    }]
+
     return (
       <div className="animated fadeIn">
        <p>Order List</p>
-
+        <div className={"bg-white"}>
+          <ReactTable
+            data={data}
+            columns={columns}
+            defaultPageSize = {5}
+          />
+        </div>
       </div>
     );
   }
